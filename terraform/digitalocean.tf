9b26fc1532b8cb4107ce2c3cf544093d8c614008
Juan Antonio F.M <juanantoniofm.11@gmail.com>
2020-12-07 03:14:54 +0100

    generating ssh config dinamically

--- terraform/digitalocean.tf
+++ terraform/digitalocean.tf
@@ -13,6 +13,9 @@ provider "digitalocean" {
   token = var.api_token
 }
 
+provider "local" {
+  version = "2.0.0"
+}
 
 resource "digitalocean_droplet" "web" {
   image              = "ubuntu-20-04-x64"
@@ -23,6 +26,24 @@ resource "digitalocean_droplet" "web" {
   ipv6               = true
   private_networking = true
   user_data          = file("user-data.yaml")
+}
+
+resource "local_file" "sshconf" {
+  content  = <<-EOT
+        Host jatos-root
+            HostName ${digitalocean_droplet.web.ipv4_address}
+            Port 22
+            User root
+            EscapeChar none
+        
+        Host jatos-web
+            HostName ${digitalocean_droplet.web.ipv4_address}
+            Port 22022
+            User admin
+            IdentityFile ../.secrets/admin
+            EscapeChar none
+        EOT
+  filename = "${path.module}/ssh_config.tf"
 }
 
 output "public_ip" {
